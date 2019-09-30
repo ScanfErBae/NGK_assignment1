1f8a5187c32f1ddbc93324eb7f1a6db9b3e9dd3f
scanferbae <201710715@post.au.dk>
2019-09-30 09:45:13 +0200

    Opdateret server kode

--- Exercise6/file_server.py
+++ Exercise6/file_server.py
@@ -2,6 +2,8 @@ import sys
 import socket
 from lib import Lib
 from socket import *
+import math
+
 HOST = ''
 PORT = 9000
 BUFSIZE = 1000
@@ -17,12 +19,21 @@ def main(argv):
         connectionSocket, addr = serverSocket.accept()
         file = Lib.readTextTCP(connectionSocket)
         filename = Lib.extractFilename(file)
+        #f = open(filename, "rb")
         size = str(Lib.check_File_Exists(filename))
         Lib.writeTextTCP(size, connectionSocket)
+        filesize = int(size)
+        chunks = int(math.ceil(filesize/BUFSIZE))
+        with open(filename, 'rb') as f:
+            dataToSend = str(f.read(BUFSIZE))
+            Lib.writeTextTCP(dataToSend, connectionSocket)
+        #for i in range (0, chunks):
+           # dataToSend = unicode(f.read(BUFSIZE), "UTF-8")
+         #   Lib.writeTextTCP(dataToSend, connectionSocket)
         connectionSocket.close()
-    
-    
-
+        f.close()
+    serverSocket.close()
+                   
 
 def sendFile(fileName,  fileSize,  conn):
     pass
