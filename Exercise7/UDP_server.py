bcdd4faafb6da41c0cc3c0c54e63ab5e106552cb
scanferbae <201710715@post.au.dk>
2019-09-30 12:16:12 +0200

    Server fungerer delvist, men printer selv filindhold ud

--- Exercise7/UDP_server.py
+++ Exercise7/UDP_server.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 from socket import *
+from lib import Lib
 
 HOST = ''
 PORT = 9000
@@ -14,24 +15,30 @@ def main(argv):
     
     while 1:
         message, clientAddress = serverSocket.recvfrom(2048)
+        print message
         if message in ('u', 'U'):
-            filename = Lib.extractFilename('/proc/uptime')
-            size = str(Lib.check_File_Exists(filename))
-            sendFile(filename, size, serverSocket)
+            serverSocket.sendto ('/proc/uptime', ('', PORT))
+            with open('/proc/uptime', "rb") as f:
+                serverSocket.sendto(f.read(), ('', PORT))
+            f.close()
+                
+                
+            #filename = Lib.extractFilename('/proc/uptime')
+            #size = str(Lib.check_File_Exists(filename))
+            #sendFile(filename, size, serverSocket)
         if message in ('l', 'L'):
-            filename = Lib.extractFilename('/proc/loadavg')
-            size = str(Lib.check_File_Exists(filename))
-            sendFile(filename, size, serverSocket)
+            serverSocket.sendto ('/proc/loadavg', ('', PORT))
+            with open('/proc/loadavg', "rb") as f:
+                serverSocket.sendto(f.read(), ('', PORT))
+            f.close()
+            
+            #filename = Lib.extractFilename('/proc/loadavg')
+            #size = str(Lib.check_File_Exists(filename))
+            #sendFile1(filename, size, serverSocket)
         
     serverSocket.close()
                    
 
-def sendFile(fileName,  fileSize,  conn):
-    Lib.writeTextTCP(fileSize, conn)
-    with open(fileName, "rb") as f:
-        conn.send(f.read())
-    conn.close()
-    f.close()
     
 if __name__ == "__main__":
     main(sys.argv[1:])
