fe037baa6a598f89aaf77d3e8729b6589f61ae52
scanferbae <201710715@post.au.dk>
2019-10-02 10:52:07 +0200

    ny client kode

--- Exercise7/UDP_client.py
+++ Exercise7/UDP_client.py
@@ -7,11 +7,12 @@ BUFSIZE = 1000
 def main(argv):
 	# TO DO Your Code
     clientSocket = socket(AF_INET, SOCK_DGRAM)
-    sentence = raw_input("Input l or u")
-    clientSocket.sendto(message, ('10.0.0.1', PORT))
+    while 1:
+        sentence = raw_input("Input l or u:")
+        clientSocket.sendto(sentence, ('10.0.0.1', PORT))
 
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    print modifiedMessage
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        print modifiedMessage
 
     clientSocket.close()
     
