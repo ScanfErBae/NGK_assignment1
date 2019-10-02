02938db834b76b13ff619ba3b6a1ed7ec7e86dc6
scanferbae <201710715@post.au.dk>
2019-10-02 11:27:14 +0200

    client virker!

--- Exercise7/UDP_client.py
+++ Exercise7/UDP_client.py
@@ -8,9 +8,9 @@ def main(argv):
 	# TO DO Your Code
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     while 1:
+            
         sentence = raw_input("Input l or u:")
         clientSocket.sendto(sentence, ('10.0.0.1', PORT))
-
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
         print modifiedMessage
 
